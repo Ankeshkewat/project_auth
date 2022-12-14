2720574767599644448f3aeeb548a67894cdfab8
Ankeshkewat <ankeshkewat966@gmail.com>
2022-12-14 21:27:04 +0530

    Add cors origin

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const {connection}=require('./config/db')
 
 const app=express();
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:"*"
+}))
 
 app.post('/signup',validator,userRouter)
 app.post('/login',userRouter)
